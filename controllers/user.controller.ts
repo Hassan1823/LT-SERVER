a03ac821f725a2a31a3ad6cce62ceb8382b3b2e1
Hassan1823 <hassan.zaib223@.com>
2024-01-30 19:57:21 +0500

    clearing bugs

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -240,7 +240,7 @@ export const updateAccessToken = CatchAsyncError(
         { id: user._id },
         process.env.ACCESS_TOKEN as string,
         {
-          expiresIn: "30m",
+          expiresIn: "3d",
         }
       );
 
